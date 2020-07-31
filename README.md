121d5f5a7037a5b8e6190c282121608780106c05
Eric Horodyski <horodyski@ionic.io>
2020-07-31 09:35:18 -0400

    Issue reproduction.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ionic Framework React Data Attributes Issue
+
+## Issue
+
+If `data` attributes are added to Ionic Framework components, they will not render in the DOM tree unless a string value is assigned to them.
+
+The following `data` attributes will not render in the DOM:
+
+- `data-number-value={42}`
+- `data-no-value-needed`
+
+However, the following data attribute will render in the DOM:
+
+- `data-explicit-string-value="myValue"`
+
+When using standard HTML tags, the data attributes will be rendered in the DOM.
+
+## Relevant Code
+
+`src/components/ExploreContainer.tsx` has been modified to reflect the issue listed above, capturing the behavior on both Ionic Components and standard HTML tags.
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import './ExploreContainer.css';
+import React from "react";
+import "./ExploreContainer.css";
+import { IonLabel } from "@ionic/react";
 
-interface ContainerProps { }
+interface ContainerProps {}
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonLabel
+        data-number-value={42}
+        data-no-value-needed
+        data-explicit-string-value="myValue"
+      >
+        <strong>Ready to create an app?</strong>
+      </IonLabel>
+      <p
+        data-number-value={42}
+        data-no-value-needed
+        data-explicit-string-value="myValue"
+      >
+        Start with Ionic{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://ionicframework.com/docs/components"
+        >
+          UI Components
+        </a>
+      </p>
     </div>
   );
 };
